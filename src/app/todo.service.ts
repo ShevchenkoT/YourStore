a9d864806ea3cc4733f432af02cdc3d3d6cbec25
ShevchenkoT <tarashevchuk@gmail.com>
2021-06-19 04:55:27 +0300

    debug some errors

---
+++ src/app/todo.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from './cart-favorites.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  constructor(public http: HttpClient) { }
+
+  addTodos(): Observable<Product[]> {
+    return this.http.get<Product[]>('/assets/mock-data/products.json')
+  }
+}
