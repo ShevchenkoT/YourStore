81cb7459ba03424ba87268917b2ebd9320c57f2c
ShevchenkoT <tarashevchuk@gmail.com>
2021-07-10 16:49:30 +0300

    add rotate button to page order

--- src/app/admin/orders/orders.component.ts
+++ src/app/admin/orders/orders.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Order, Product } from 'src/app/shared/interfaces';
 import { OrderService } from 'src/app/shared/service/order.service';
@@ -5,15 +6,32 @@ import { OrderService } from 'src/app/shared/service/order.service';
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.scss'],
+  animations: [
+    trigger('rotate', [
+      state('start', style({ transform: 'rotate(0deg)' })),
+      state('end', style({ transform: 'rotate(90deg)' })),
+      transition('start <=> end', animate(450)),
+      // transition('end => start', animate('800ms ease-in-out')),
+    ])
+  ]
 })
 export class OrdersComponent implements OnInit {
-
+  toggle = true
+  boxState = 'start'
   orders: Order[] = []
   constructor(
     private orderService: OrderService
   ) { }
 
+  animate() {
+    this.boxState = this.boxState === 'end' ? 'start' : 'end'
 
+  }
+
+
+  rotate(event: any) {
+    event.target.classList.contains('rotate') ? event.target.classList.remove('rotate') : event.target.classList.add('rotate')
+  }
   ngOnInit() {
     this.orderService.getAll().subscribe((orders: Order[]) => {
       this.orders = orders
