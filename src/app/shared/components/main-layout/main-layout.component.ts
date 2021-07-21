2c99910570af96570e5db7758b5dcb60c086f70f
ShevchenkoT <tarashevchuk@gmail.com>
2021-07-21 23:00:42 +0300

    white theme in main layout is done

--- src/app/shared/components/main-layout/main-layout.component.ts
+++ src/app/shared/components/main-layout/main-layout.component.ts
@@ -54,13 +54,13 @@ import {
       document.body.style.setProperty('--second-color', "#272727")
       document.body.style.setProperty('--text-color', "#fff")
       document.body.style.setProperty('--body-color', "#000")
-      event.target.parentNode.parentNode.classList.remove("color-invert")
+      event.target.parentNode.parentNode.parentNode.classList.remove("color-invert")
     } else {
       document.body.style.setProperty('--first-color', "#f0f2f5")
       document.body.style.setProperty('--second-color', "#c7c7c7")
       document.body.style.setProperty('--text-color', "#000")
       document.body.style.setProperty('--body-color', "#fbfbfd")
-      event.target.parentNode.parentNode.classList.add("color-invert")
+      event.target.parentNode.parentNode.parentNode.classList.add("color-invert")
     }
 
   }
